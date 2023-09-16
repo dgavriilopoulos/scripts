6afe0cffb40015822eb4d77e45a28b5c1306f589
dgavriilopoulos <144386901+dgavriilopoulos@users.noreply.github.com>
2023-09-15 22:13:18 -0500

    \

---
+++ enumeration_networkServices.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import wmi
+import sys
+
+python_path = sys.executable
+
+def running_processes():
+    f = wmi.WMI()
+
+    for process in f.Win32_Process():
+        print(f"{process.ProcessId:10} {process.Name} {process.ExecutablePath}")
+
+
+if __name__ == "__main__":
+
+    print(f'Python Interpreter Path: {python_path}')
+
+    print("\n","ProcessID", "ProcessName","ExecutablePath")
+
+    list_running_processes = running_processes()
+    print(list_running_processes)
